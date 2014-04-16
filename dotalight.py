eaab9020f0431e78d37458b646e7756bfa56f7b4
Pei Wang <pxw172@ist.psu.edu>
2014-04-15 23:27:14 -0400

    Implement the experimental WSGI dispatching mechanism.

--- dotalight.py
+++ dotalight.py
@@ -13,12 +13,22 @@ import html
 def application(environ, start_response):
     reload(html)
 
-    path = environ['PATH_INFO']  # The trailing part
-
     status = '200 OK'
-    output = html.HTML['portal']
 
-    response_headers = [('Content-type', 'text/html'),
+    url_path = environ['PATH_INFO'][1:]  # The trailing part of the requested URL
+
+    if url_path == '':
+        content_type = 'text/html'
+        output = html.HTML['portal']
+    elif url_path.find('/') == -1 and url_path.startswith('account_'):
+        vanity_url = url_path[8:]
+        output = '<html><head><title>Test Page</title></head><body><p>Requested User Profile: %s</p></body></html>' % (vanity_url)
+        content_type = 'text/html'
+    else:
+        output = 'Unkown Request!'
+        content_type = 'text/plaintext'
+
+    response_headers = [('Content-Type', content_type),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
 
