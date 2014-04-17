f8c37767afec8829623e6ce3d00373c6d751b010
Pei Wang <pxw172@ist.psu.edu>
2014-04-16 20:53:32 -0400

    Add form data parsing code.

--- dotalight.py
+++ dotalight.py
@@ -1,18 +1,36 @@
 import os
 import sys
+import cgi
+import jinja2
 
 PYPATH = os.path.dirname(os.path.realpath(__file__))
 if PYPATH not in sys.path:
     sys.path.append(PYPATH)
 
-import cgi
-import jinja2
 import database
 import html
 
 def application(environ, start_response):
+    # Reload html page everytime. This is good for debugging.
     reload(html)
+    
+    method = environ['REQUEST_METHOD'].upper()
+    if method == 'POST':
+        # the environment variable CONTENT_LENGTH may be empty or missing
+        request_body_size = int(environ.get('CONTENT_LENGTH', '0'))
 
+        # When the method is POST the query string will be sent
+        # in the HTTP request body which is passed by the WSGI server
+        # and sent via the wsgi.input environment variable.
+        request_body = environ['wsgi.input'].read(request_body_size)
+        # post_data is a dictory storing data from client
+        post_data = cgi.parse_qs(request_body)
+    elif method == 'GET':
+        get_data = cgi.parse_qs(environ['QUERY_STRING'])
+    else:
+        # This branch should be unreachable 
+        pass
+    
     status = '200 OK'
 
     url_path = environ['PATH_INFO'][1:]  # The trailing part of the requested URL
